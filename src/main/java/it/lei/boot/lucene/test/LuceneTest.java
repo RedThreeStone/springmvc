62a45a2f1f4ed39521474585e3b0adee6d6fe8f0
RedThreeStone <837387518@qq.com>
2018-10-31 18:22:42 +0800

    Lucene查询

--- src/main/java/it/lei/boot/lucene/test/LuceneTest.java
+++ src/main/java/it/lei/boot/lucene/test/LuceneTest.java
@@ -2,10 +2,11 @@ package it.lei.boot.lucene.test;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.document.*;
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import org.junit.Test;
@@ -61,6 +62,86 @@ public class LuceneTest {
         writer.close();
     }
 
+    /**
+     * termquery精确查找
+     */
+    private void termQueryTest() throws IOException {
+        String indexPath="D:\\studyDatas\\Lucene\\2";
+        DirectoryReader reader=null;
+        //获取查找对象
+        IndexSearcher indexSearcher = getIndexSearcher(indexPath);
+        try {
+            //精确匹配类
+            Query query = new TermQuery(new Term("fileName","chrome_child.dll.sig"));
+            TopDocs topDocs = indexSearcher.search(query, 10);
+            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+            //打印内容
+            printContent(indexSearcher, scoreDocs);
+        }catch (Exception e){
+            indexSearcher.getIndexReader().close();
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 查询所有
+     * @throws IOException
+     */
+    private void  allQuery()throws  IOException{
+        String indexPath="D:\\studyDatas\\Lucene\\2";
+        DirectoryReader reader=null;
+        //获取查找对象
+        IndexSearcher indexSearcher = getIndexSearcher(indexPath);
+        try {
+            //查询所有类
+            MatchAllDocsQuery query = new MatchAllDocsQuery();
+            TopDocs topDocs = indexSearcher.search(query, 10);
+            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+            //打印内容
+            printContent(indexSearcher, scoreDocs);
+        }catch (Exception e){
+            indexSearcher.getIndexReader().close();
+            e.printStackTrace();
+        }
+    }
+
+    private void  numberQueryTest()throws  Exception{
+        String indexPath="D:\\studyDatas\\Lucene\\2";
+        DirectoryReader reader=null;
+        //获取查找对象
+        IndexSearcher indexSearcher = getIndexSearcher(indexPath);
+        try {
+            //范围查询
+            NumericRangeQuery<Long> size = NumericRangeQuery.newLongRange("size", 100L, 100000L, false, true);
+            TopDocs topDocs = indexSearcher.search(size, 10);
+            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+            //打印内容
+            printContent(indexSearcher, scoreDocs);
+        }catch (Exception e){
+            indexSearcher.getIndexReader().close();
+            e.printStackTrace();
+        }
+    }
+
+    private IndexSearcher getIndexSearcher(String indexPath) throws IOException {
+        DirectoryReader reader;
+        FSDirectory dir = FSDirectory.open(new File(indexPath));
+        reader = DirectoryReader.open(dir);
+        return new IndexSearcher(reader);
+    }
+
+    private void printContent(IndexSearcher indexSearcher, ScoreDoc[] scoreDocs) throws IOException {
+        for(ScoreDoc scoreDoc:scoreDocs){
+            //id
+            int doc = scoreDoc.doc;
+            Document document = indexSearcher.doc(doc);
+            System.out.println(document.get("filePath"));
+            System.out.println(document.get("fileName"));
+            System.out.println(document.get("content"));
+        }
+    }
+
+
     private IndexWriter getWriter(String dirPath) throws IOException {
         File file = new File(dirPath);
         FSDirectory directory = FSDirectory.open(file);
@@ -68,5 +149,6 @@ public class LuceneTest {
         IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_4,ikAnalyzer);
         return new IndexWriter(directory,config);
     }
+
 
 }
