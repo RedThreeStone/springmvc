0dff7052a2ce8bdeb893d6483256d0f2bdc14c6a
RedThreeStone <837387518@qq.com>
2018-11-01 11:34:58 +0800

    lucene学习最终版

--- src/main/java/it/lei/boot/lucene/test/LuceneTest.java
+++ src/main/java/it/lei/boot/lucene/test/LuceneTest.java
@@ -6,6 +6,8 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
@@ -14,6 +16,7 @@ import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class LuceneTest {
     @Test
@@ -29,7 +32,10 @@ public class LuceneTest {
             String filePath=file.getAbsolutePath();
             String content = FileUtils.readFileToString(file, "UTF-8");
             long size = FileUtils.sizeOf(file);
-            document.add(new StringField("filePath",filePath, Field.Store.YES));
+            //设置权重
+            StringField filePathTerm = new StringField("filePath", filePath, Field.Store.YES);
+            filePathTerm.setBoost(100L);
+            document.add(filePathTerm);
             document.add(new LongField("size",size, Field.Store.YES));
             document.add(new TextField("fileName",fileName, Field.Store.YES));
             document.add(new TextField("content",content, Field.Store.YES));
@@ -72,7 +78,10 @@ public class LuceneTest {
         IndexSearcher indexSearcher = getIndexSearcher(indexPath);
         try {
             //精确匹配类
-            Query query = new TermQuery(new Term("fileName","chrome_child.dll.sig"));
+            Term fileNameTerm = new Term("fileName", "chrome_child.dll.sig");
+            Query query = new TermQuery(fileNameTerm);
+            //设置权重
+            query.setBoost(200L);
             TopDocs topDocs = indexSearcher.search(query, 10);
             ScoreDoc[] scoreDocs = topDocs.scoreDocs;
             //打印内容
@@ -105,6 +114,10 @@ public class LuceneTest {
         }
     }
 
+    /**
+     * 范围查找
+     * @throws Exception
+     */
     private void  numberQueryTest()throws  Exception{
         String indexPath="D:\\studyDatas\\Lucene\\2";
         DirectoryReader reader=null;
@@ -114,6 +127,96 @@ public class LuceneTest {
             //范围查询
             NumericRangeQuery<Long> size = NumericRangeQuery.newLongRange("size", 100L, 100000L, false, true);
             TopDocs topDocs = indexSearcher.search(size, 10);
+            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+            //打印内容
+            printContent(indexSearcher, scoreDocs);
+        }catch (Exception e){
+            indexSearcher.getIndexReader().close();
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 多条件查找
+     * @throws Exception
+     */
+    @Test
+    private void boolQueryTest()throws  Exception{
+        String indexPath="D:\\studyDatas\\Lucene\\2";
+        DirectoryReader reader=null;
+        //获取查找对象
+        IndexSearcher indexSearcher = getIndexSearcher(indexPath);
+        try {
+
+            Query query = new TermQuery(new Term("fileName","chrome_child.dll.sig"));
+            NumericRangeQuery<Long> size = NumericRangeQuery.newLongRange("size", 100L, 100000L, false, true);
+            BooleanQuery booleanQuery = new BooleanQuery();
+            //设置boost
+            size.setBoost(200L);
+            booleanQuery.add(size, BooleanClause.Occur.MUST);
+            booleanQuery.add(query, BooleanClause.Occur.MUST);
+//            1、MUST和MUST表示“与”的关系，即“并集”。
+//            2、MUST和MUST_NOT前者包含后者不包含。
+//            3、MUST_NOT和MUST_NOT没意义
+//            4、SHOULD与MUST表示MUST，SHOULD失去意义；
+//            5、SHOUlD与MUST_NOT相当于MUST与MUST_NOT。
+//            6、SHOULD与SHOULD表示“或”的概念。
+
+
+            TopDocs topDocs = indexSearcher.search(booleanQuery, 10);
+            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+            //打印内容
+            printContent(indexSearcher, scoreDocs);
+        }catch (Exception e){
+            indexSearcher.getIndexReader().close();
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * parser多域查询
+     */
+    @Test
+    public  void multiFieldQueryParserTest() throws IOException {
+        String indexPath="D:\\studyDatas\\Lucene\\2";
+        DirectoryReader reader=null;
+        //获取查找对象
+        IndexSearcher indexSearcher = getIndexSearcher(indexPath);
+        try {
+            String[] fields = {"fileName", "content"};
+            HashMap<String, Float> boostMap = new HashMap<>();
+            boostMap.put("fileName",100f);
+            boostMap.put("content",200f);
+            MultiFieldQueryParser queryParser = new MultiFieldQueryParser(Version.LUCENE_4_10_4,
+                    fields,new IKAnalyzer(),boostMap);
+            //fileName='6666' or content='6666'
+            Query query = queryParser.parse("666");
+            TopDocs topDocs = indexSearcher.search(query, 10);
+            ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+            //打印内容
+            printContent(indexSearcher, scoreDocs);
+        }catch (Exception e){
+            indexSearcher.getIndexReader().close();
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 多条件parser查询
+     */
+    @Test
+    public void boolParserTest() throws Exception{
+        String indexPath="D:\\studyDatas\\Lucene\\2";
+        DirectoryReader reader=null;
+        //获取查找对象
+        IndexSearcher indexSearcher = getIndexSearcher(indexPath);
+        try {
+            //第二个参数为默认域
+            QueryParser queryParser = new QueryParser(Version.LUCENE_4_10_4,"fileName",new IKAnalyzer());
+            String queryString ="fileName:6666 and fileSize:[20 TO 100]" ;
+            Query query = queryParser.parse(queryString);
+            TopDocs topDocs = indexSearcher.search(query, 10);
             ScoreDoc[] scoreDocs = topDocs.scoreDocs;
             //打印内容
             printContent(indexSearcher, scoreDocs);
